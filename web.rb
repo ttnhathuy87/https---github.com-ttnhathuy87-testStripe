606ab2ad20ec7728971f099c0b1f89b8684883ee
ttnhathuy87 <huy@appiphany.com.au>
2015-12-09 17:17:42 +0700

    Update web.rb

--- web.rb
+++ web.rb
@@ -17,17 +17,17 @@ post '/charge' do
   token = params[:stripeToken]
 
   # Create the charge on Stripe's servers - this will charge the user's card
-  begin
-    charge = Stripe::Charge.create(
-      :amount => params[:amount], # this number should be in cents
-      :currency => "usd",
-      :card => token,
-      :description => "Example Charge"
-    )
-  rescue Stripe::CardError => e
-    status 402
-    return "Error creating charge."
-  end
+ # begin
+  #  charge = Stripe::Charge.create(
+ #     :amount => params[:amount], # this number should be in cents
+  #    :currency => "usd",
+ #     :card => token,
+  #    :description => "Example Charge"
+  #  )
+ # rescue Stripe::CardError => e
+ #   status 402
+#    return "Error creating charge."
+#  end
 
   status 200
   return "Order successfully created"
